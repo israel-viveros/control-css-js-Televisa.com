7421db905e4d89498ebbfc135637d36d39f98ca3
gabrielesqueda <jose.esqueda@esmas.net>
2014-01-14 01:57:55 -0600

    Ajuste Home #214
    Ajuste en el cursor de la pieza

--- js/indices/file.js
+++ js/indices/file.js
@@ -1768,3 +1768,10 @@ $(function() {
         LugarSite.where();
     }    
 });
+
+$(document).ready(function() {	
+	$('.content-stage-description').click(function(e) {
+		var url = $('.TitleEdiStageContainerLarge a',this).attr('href');
+		window.location.href = url;
+	});
+});
