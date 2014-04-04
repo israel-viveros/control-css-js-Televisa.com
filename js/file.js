d8cebb86bd2f1d1855cf32ccacba46c346ab5b3a
gabrielesqueda <jose.esqueda@esmas.net>
2014-04-04 17:35:27 -0600

    Ajustes mobile

--- js/file.js
+++ js/file.js
@@ -3568,7 +3568,7 @@ $(document).ready(function() {
 });$(document).ready(function() {
     var $nav_footer_retorna = 0;
     $('.nav_footer_01 .icon-social').bind('touchstart', function(e) {
-        e.preventDefault();
+        //e.preventDefault();
     });
     $('.nav_footer_01 .back').click(function(e) {
         e.preventDefault();
